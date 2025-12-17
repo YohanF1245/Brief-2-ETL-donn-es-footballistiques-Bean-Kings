ec3a50d1377d1efa2ec91eff5afaac7c532763cc
YohanF1245 <yohan.ferdin@hotmail.fr>
2025-12-17 15:00:10 +0100

    fix(etl_2014): update import path for etl_inserter_2014 module

--- etl/etl_2014.py
+++ etl/etl_2014.py
@@ -1,6 +1,16 @@
 import duckdb
-import etl_inserter_2014 as inserter
+import etl.etl_inserter_2014 as inserter
 import pandas as pd
 from unidecode import unidecode
 import geonamescache
 import logging
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
+gc = geonamescache.GeonamesCache()
+df = pd.read_csv('./data/WorldCupMatches2014.csv', sep=";", encoding='iso-8859-1') 
+logger = logging.getLogger("ETL")
+
+cities =  gc.get_cities()
+countries = gc.get_countries()
